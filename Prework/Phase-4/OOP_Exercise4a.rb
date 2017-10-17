00e92b53f891bb44a4b668b567ab5e9e1d94aa53
Uch-iha <32764551+Uch-iha@users.noreply.github.com>
2017-10-16 21:11:49 -0500

    Delete OOP_Exercise4a.rb

--- Prework/Phase-4/OOP_Exercise4a.rb
+++
@@ -1,6 +0,0 @@
-# Create the Dog class in your text editor as it’s been presented in the video.
-# What happens if you eliminate the return keyword from the name, breed, and age methods?
-# That is, what would happen if the name method looked like this:
-# def name
-#   @dogs_name
-# end
